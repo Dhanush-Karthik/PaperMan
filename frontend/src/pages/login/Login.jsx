5c2a8b7925d2506117680150c90cb25615bd9edf
Dhanush-Karthik <dhanushkarthikgit@gmail.com>
2023-06-30 02:50:07 +0530

    login page

--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -1,15 +1,61 @@
 import React from "react";
 import Navbar from "../../components/Navbar";
-
+import { BsFillPersonVcardFill } from "react-icons/bs";
+import { MdEmail } from "react-icons/md";
+import { AiFillEye } from "react-icons/ai";
+import "./Login.css";
 const Login = () => {
   return (
     <div className="login-container">
       <Navbar />
-      <h3>Start for free</h3>
-      <h2>Create new ccount.</h2>
-      <p>Already a Member? <span>Log In</span></p>
-      <form>
-
+      <h3 className="sub-heading1">START FOR FREE</h3>
+      <h1 className="main-heading">Create new account<span>.</span></h1>
+      <p className="sub-heading2">
+        Already a Member? <span>Log In</span>
+      </p>
+      <form className="login-form">
+        <div className="name-container">
+          <div className="input-container">
+            <div className="input-section">
+              <p className="label">First Name</p>
+              <input className="form-input" />
+            </div>
+            <div className="form-icon">
+              <BsFillPersonVcardFill size={"20px"} />
+            </div>
+          </div>
+          <div className="input-container">
+            <div className="input-section">
+              <p className="label">Last Name</p>
+              <input className="form-input" />
+            </div>
+            <div className="form-icon">
+              <BsFillPersonVcardFill size={"20px"} />
+            </div>
+          </div>
+        </div>
+        <div className="input-container">
+          <div className="input-section">
+            <p className="label">Email</p>
+            <input className="form-input" />
+          </div>
+          <div className="form-icon">
+            <MdEmail size={"22px"} />
+          </div>
+        </div>
+        <div className="input-container">
+          <div className="input-section">
+            <p className="label">Password</p>
+            <input type="password" className="form-input" />
+          </div>
+          <div className="form-icon">
+            <AiFillEye size={"22px"} />
+          </div>
+        </div>
+        <div className="form-button-container">
+          <button className="form-button back">Go back</button>
+          <button className="form-button create">Create account</button>
+        </div>
       </form>
     </div>
   );
