cb84a3068f68c3ee29387dc9b57b614b40544e84
raghu-nandan10 <trafalgardwaterlaw393@gmail.com>
2023-06-29 23:42:37 +0530

    init

---
+++ frontend/src/App.js
@@ -0,0 +1,18 @@
+
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import './App.css';
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <div className="App">
+     <BrowserRouter>
+     <Routes>
+      <Route path='/' element={<Navbar/>} />
+     </Routes>
+     </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
