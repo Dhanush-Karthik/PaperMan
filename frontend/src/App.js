91b47b3365b12658cc7a7028c4c6f170bca348c2
Dhanush-Karthik <dhanushkarthikgit@gmail.com>
2023-06-30 00:39:51 +0530

    login page added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@
 import { BrowserRouter,Routes,Route } from 'react-router-dom';
 import './App.css';
 import Navbar from './components/Navbar';
-import Login from './pages/Login';
+import Login from './pages/login/Login';
 
 function App() {
   return (
---
+++ frontend/src/pages/login/Login.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Navbar from "../../components/Navbar";
+
+const Login = () => {
+  return (
+    <div className="login-container">
+      <Navbar />
+      <h3>Start for free</h3>
+      <h2>Create new ccount.</h2>
+      <p>Already a Member? <span>Log In</span></p>
+      <form>
+
+      </form>
+    </div>
+  );
+};
+
+export default Login;
